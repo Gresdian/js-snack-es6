b163cc4fc2d36f1febd8989de7cfe86cef05544c
Gresdian <chris_da98@outlook.it>
2023-12-06 17:48:13 +0100

    Snack 2

--- js/main.js
+++ js/main.js
@@ -31,5 +31,43 @@ bike.forEach((elem) => {
         console.log(elem);
     }
 })
- 
+
+//SNACK 2
+
+const footballteam = [
+    {
+        name : 'Juventus',
+        point : 0,
+        fouls : 0
+    },
+    {
+        name : 'Inter',
+        point : 0,
+        fouls : 0
+    },
+    {
+        name : 'Milan',
+        point : 0,
+        fouls : 0
+    },
+    {
+        name : 'Napoli',
+        point : 0,
+        fouls : 0
+    },
+    {
+        name : 'Roma',
+        point : 0,
+        fouls : 0
+    },
+];
+console.log(footballteam);
+footballteam.forEach((elem) => {
+    elem.point = Math.floor(Math.random()*40 + 1);
+    elem.fouls = Math.floor(Math.random() * 10 + 1);
+    
+})
+
+let teams_fouls = footballteam.map(({name , fouls}) =>  ({name, fouls}));
+console.log(teams_fouls);
 
